9ec575bb91396ccbe1145197ff18a6f3cc12478a
WhereIsTheDog <mrfufu888@gmail.com>
2019-06-07 22:00:40 +0800

    Create App.vue

---
+++ Web/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Show me the fruits</h1>
+    <DropImage />
+  </div>
+</template>
+
+<script>
+import DropImage from './components/DropImage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    DropImage
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
